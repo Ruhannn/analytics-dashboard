0f32f7f0264b996e23f1a8e0ac17aea3ba1d09a1
Ruhannn <kamiayaka09@gmail.com>
2024-03-29 16:46:39 +0600

    retrieve and retrieveDays added

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,6 +1,14 @@
+import { analytics } from "@/utils/analytics";
 
-export default function page() {
+const page = async () => {
+  const pageView = await analytics.retrieveDays("page-view", 1);
   return (
-    <div>hello</div>
-  )
-}
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+
+      </div>
+    </div>
+  );
+};
+
+export default page;
